2017e070d108d94f0692879a3c4420cb82ce62b7
Dennis Akagha <d.akagha20@gmail.com>
2024-12-20 16:25:43 +0100

    add other fields to model

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blog.models import Post
 
-# Register your models here.
+
+# customize the admin
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug", "author", "publish", "status"]
