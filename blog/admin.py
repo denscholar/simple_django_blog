ec505a61038ae2a8ff07fdd638c79838c18481bb
Dennis Akagha <d.akagha20@gmail.com>
2024-12-21 23:19:04 +0100

    add commit

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,11 @@ from blog.models import Post
 # customize the admin
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ["title", "slug", "author", "publish", "status"]
+    list_display = ("title", "slug", "author", "publish", "status")
+    list_filter = ("created", "author", "publish", "status")
+    search_fields = ("title", "body")
+    prepopulated_fields = {"slug": ("title",)}
+    # raw_id_fields = ("author",)
+    date_hierarchy = 'publish'
+    ordering = ("status", "publish")
+    show_facets = admin.ShowFacets.ALWAYS
