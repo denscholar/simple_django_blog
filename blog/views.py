661754d5c925f6a75b3e63ddcb65465fdb612d41
Dennis Akagha <d.akagha20@gmail.com>
2024-12-22 23:05:00 +0100

    add view to project

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts': posts,}
+    
+    return render(request, 'blog/post/list.html', context)
+
+def post_detail(request, id):
+    try:
+        post = Post.published.get(id=id)
+        context = {'post': post}
+    except Post.DoesNotExist:
+        Http404("No Post found.") 
+    return render(request, 'blog/post/detail.html', context)
+
