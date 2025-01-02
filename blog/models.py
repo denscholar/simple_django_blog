8d01bd8a6f8a0c8dd891338a6867286921f5775d
Dennis Akagha <d.akagha20@gmail.com>
2025-01-02 14:23:36 +0100

    add a url for seo

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="blog_posts"
     )
@@ -24,15 +24,22 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT)
-    objects = models.Manager() # The default manager
-    published = PublishedManager() # Our custom manager.
+    objects = models.Manager()  # The default manager
+    published = PublishedManager()  # Our custom manager.
 
     class Meta:
         ordering = ["-publish"]
 
-
     def __str__(self):
         return self.title
-    
+
     def get_absolute_url(self):
-        return reverse('blog:post_detail', args=[self.id])
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
