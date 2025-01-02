f30536ee5d1dda5ebf9ad9d74ad87f5ba2982cb0
Dennis Akagha <d.akagha20@gmail.com>
2025-01-02 14:00:22 +0100

    add a url for seo

--- blog/views.py
+++ blog/views.py
@@ -12,8 +12,15 @@ def post_list(request):
     return render(request, "blog/list.html", context)
 
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id)
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(
+        Post,
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+    )
     context = {"post": post}
 
     # try:
