19fb6c8f7b624cdd7f49c47824a76286fdcf849f
Dennis Akagha <d.akagha20@gmail.com>
2025-01-07 16:28:25 +0100

    add class based

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
+from blog.views import PostListView
 from . import views
+
 
 
 app_name = 'blog'
 
 
 urlpatterns = [
-    path('blog/', views.post_list, name='post_list'),
+    # path('blog/', views.post_list, name='post_list'),
+    path('blog/', PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,25 +2,26 @@ from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 
-def post_list(request):
-    post_list = Post.published.all()
-    paginator = Paginator(
-        post_list, 2
-    )  # this takes the post list and how many post in a page
-    page_number = request.GET.get("page", 1)
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(
+#         post_list, 2
+#     )  # this takes the post list and how many post in a page
+#     page_number = request.GET.get("page", 1)
 
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer get the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-    context = {"posts": posts}
-    return render(request, "blog/list.html", context)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer get the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range get last page of results
+#         posts = paginator.page(paginator.num_pages)
+#     context = {"posts": posts}
+#     return render(request, "blog/list.html", context)
 
 
 def post_detail(request, year, month, day, post):
@@ -40,3 +41,13 @@ def post_detail(request, year, month, day, post):
     # except Post.DoesNotExist:
     #     Http404("No Post found.")
     return render(request, "blog/detail.html", context)
+
+
+
+# create a new posts list using class-based views
+class PostListView(ListView):
+    # alternative post list view
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/list.html'
