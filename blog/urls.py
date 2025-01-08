89a8aefbe5b11260315cfc34b5ae9069b377a602
Dennis Akagha <d.akagha20@gmail.com>
2025-01-08 13:26:03 +0100

    add share email template

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ app_name = 'blog'
 urlpatterns = [
     # path('blog/', views.post_list, name='post_list'),
     path('blog/', PostListView.as_view(), name='post_list'),
-    path('/<int:post_id>/share/', views.post_share(),  name='post_share'),
+    path('/<int:post_id>/share/', views.post_share,  name='post_share'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -57,17 +57,18 @@ class PostListView(ListView):
 # post Share
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    sent = False
     if request.method == "POST":
         # form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():  # Form fields passed validation
             cd = form.cleaned_data  # ... send email
-            post_url = request.build_absolute_url(post.get_absolute_url())
+            post_url = request.build_absolute_uri(post.get_absolute_url())
             subject = (
-                f"{cd['email']} ({cd['title']})" f"recommends you read {post.title}"
+                f"{cd['name']} ({cd['email']})" f"recommends you read {post.title}"
             )
             message = (
-                f"Read {post.title} at {post.url}\n\n"
+                f"Read {post.title} at {post_url}\n\n"
                 f"{cd['name']}'s comments: {cd['comments']}"
             )
             send_mail(
